399bc76f85c4ebbd4d32e18ff905a055881f2d99
HACK3R-AKR <80332174+HACK3R-AKR@users.noreply.github.com>
2023-07-17 00:38:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import  os
 
-BOT_TOKEN = os.environ.get("6337288649:AAFKbLcZMRunVFndNvJ8H0JVMKBnX8V8-o4")
-APP_ID = int(os.environ.get("5329569"))
-API_HASH = os.environ.get("adcfac3b2c5ec0b803fdbfa69e7cf272")
+BOT_TOKEN = os.environ.get(")
+APP_ID = int(os.environ.get(""))
+API_HASH = os.environ.get")
 
 youtube_next_fetch = 0  # time in minute
 
